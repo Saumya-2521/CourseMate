52aa9b6c0b3e2b8a1aec244c0b9ab167fc8e9929
navidfarahani <navid300@students.cs.ubc.ca>
2023-11-19 11:22:22 -0800

    added extra return statement

--- src/course.py
+++ src/course.py
@@ -4,7 +4,7 @@ class Course:
         self.courseCode = course_code
         self.sectionCode = section_code
 
-    # Returns True if the two Courses are the same
+    # Returns True if the two courses are the same
     def sameCourse(self, otherCourse): 
         if (self.dept == otherCourse.dept) & (self.courseCode == otherCourse.courseCode):
             return True
@@ -14,12 +14,18 @@ class Course:
         if (self.sectionCode == otherCourse.sectionCode):
             return True
 
+    # Returns True if the two courses and sections are the same, 
+    # but return False if the courses are the same, but the sections are different
     def sameExactCourse(self, otherCourse):
         if self.sameCourse(otherCourse):
             if self.sameSection(otherCourse):
                 return True
             else:
                 return False
+        return False
+
+
+    
 
 
     
