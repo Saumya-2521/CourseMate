1f2e2663b91cd6653a93a6f172ad5cbeeef0b5d5
Max Landon <lavamarmax@gmail.com>
2023-11-19 13:26:19 -0800

    Add file and move getRankedList to Application

---
+++ src/Application.py
@@ -0,0 +1,21 @@
+import src.Student from Student
+import pickle
+
+class Application:
+	PICKLE_FILE = "studentList.pickle"
+	def __init__(self):
+		# Load list of students from file
+		self.studentList = self.loadStudentList()
+
+	# @return the final list of ranked students for a given student
+	def getRankedList(self, student):
+		# TODO
+
+	# Creates a list of students from a file
+	# @return a list of students
+	def loadStudentList(self):
+		try:
+			with open(self.PICKLE_FILE, 'rb') as f:
+				return pickle.load(f)
+		except FileNotFoundError:
+			return []
