05481ee8d5ded0bc6394358d20f55249e9690227
Saumya Sanghvi <saumyasanghvi25@gmail.com>
2023-11-19 10:04:50 -0800

    Boilerplate for student

---
+++ src/Student.py
@@ -0,0 +1,18 @@
+from flask import Flask
+# from Course import Course
+
+class Student:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.courseList = []
+
+    # @return the final list of ranked students
+    def getRankedList(self):
+        # TODO: Implement this method
+        pass # Remove this line when you implement this method
+    
+    # @return a number above 0, denoting similarity between a student and self
+    def getOtherStudentPoints(self):        
+        # TODO: Implement this method
+        pass # Remove this line when you implement this method
